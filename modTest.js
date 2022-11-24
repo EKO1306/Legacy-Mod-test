9d3874dfb21dbbffaedbf1bd78e105ee6d8c9acd
EKO1306 <118976413+EKO1306@users.noreply.github.com>
2022-11-24 12:13:38 +0000

    Update modTest.js

--- modTest.js
+++ modTest.js
@@ -5,7 +5,7 @@ desc:'A simple example mod that adds hot peppers and hot sauce.',
 engineVersion:1,
 manifest:'myModManifest.js',
 requires:['Default dataset*'],
-sheets:{'spicySheet':'img/spicyModIconSheet.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
+sheets:{'icon':'img/iconSheet.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
 func:function()
 {
 	//The idea in this simple example mod is to add a few elements focused around hot sauce, because hot sauce is great and I use that stuff everywhere.
@@ -14,7 +14,7 @@ func:function()
 	new G.Res({
 		name:'hot pepper',
 		desc:'I hate my life.',
-		icon:[0,0,'spicySheet'],
+		icon:[0,0],
 		turnToByContext:{'eat':{'health':0.01,'happiness':0.03},'decay':{'spoiled food':0.5}},//this basically translates to : "when eaten, generate some health and happiness; when rotting, turn into either nothing or some spoiled food"
 		partOf:'food',
 		category:'food',
