4737ed8b5fb8add5f5d36f7b4a5c0b2e2258d7e4
EKO1306 <118976413+EKO1306@users.noreply.github.com>
2022-11-25 09:26:51 +0000

    Update modTest.js

--- modTest.js
+++ modTest.js
@@ -1,7 +1,7 @@
 G.AddData({
 name:'Mod Test',
 author:'Orteil',
-desc:'A simple example mod that adds hot peppers and hot sauce.',
+desc:'I don't know yet.',
 engineVersion:1,
 manifest:0,
 requires:['Default dataset*'],
@@ -14,12 +14,12 @@ func:function()
 	new G.Res({ 
 		name:'power', 
 		desc:'[power] comes in many different forms, such as steam turbines or voltage. Either way, they are all used by many machines.' +numbersInfo, 
-		limit:'power storage', 
+		limit:'power-storage', 
 		icon:[6,1], 
 		category:'essentials'
 	});
 	new G.Res({ 
-		name:'power storage', 
+		name:'power-torage', 
 		hidden = true,
 		icon:[6,1], 
 	});
@@ -32,31 +32,26 @@ func:function()
 	});
 	
 	//Then we augment the base data to incorporate our new resources :
-		//adding hot pepper as something that can be gathered from grass
 	G.getDict('grass').res['gather']['power']=3;
-		//adding a new mode to artisans so they can make hot sauce from hot peppers
 	G.getDict('firekeeper').modes['torch']={name:'Light torches',desc:'Wrap 3 flammable [herb]s around a [stick] to create a torch.',req:{'torch-making':true}};
-		//adding a new effect to artisans that handles the actual hot sauce preparing and is only active when the unit has the mode "hot sauce"
 	G.getDict('firekeeper').effects.push({type:'convert',from:{'stick':1,'herb':3},into:{'torch':1},every:3,mode:'torch'});
 	
-	//Then we add a new technology which is required by the artisans to gain access to the "hot sauce" mode :
 	new G.Tech({
 		name:'torch-making',
 		desc:'@[artisan]s can now produce [torch]es@Unlocks a new modes for [wanderer]s and [scout]s which increase searching speed using torches// by wrapping flammable [herb]s around a [stick] and setting fire to it, we create a portable light.',
-		icon:[0,1,'spicySheet'],
+		icon:[0,1],
 		cost:{'insight':10},
 		req:{'speech':true},
 	});
-	
-	//Finally, we add a trait that amplifies the benefits of consuming hot sauce; it will take on average 20 years to appear once the conditions (knowing the "Hot sauce preparing" tech) is fulfilled.
+
 	new G.Trait({
-		name:'hot sauce madness',
-		desc:'@your people appreciate [hot sauce] twice as much and will be twice as happy from consuming it.',
-		icon:[1,1,'spicySheet'],
+		name:'herbidity',
+		desc:'@your people appreciate [herb]s twice as much and will be twice as happy from consuming it.',
+		icon:[6,1],
 		chance:0.1,
 		req:{'torch-making':true},
 		effects:[
-			{type:'function',func:function(){G.getDict('hot sauce').turnToByContext['eat']['happiness']=0.2;}},//this is a custom function executed when we gain the trait
+			{type:'function',func:function(){G.getDict('herb').turnToByContext['eat']['happiness']=0.2;}},//this is a custom function executed when we gain the trait
 		],
 	});
 	
